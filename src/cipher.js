6c78f0f5a98d3be34a93ada6b22266f56de436c5
Oshin Villegas <villegasoshin25@gmail.com>
2018-05-25 16:21:37 -0500

    todofuncionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,13 @@
 window.cipher = {
   encode : () => {
-    var clave = 3
+    var llave = parseInt(document.getElementById('candado').value);
     var textoOriginal = document.getElementById("textoOriginal").value;
     var textoEncriptado = "";
 
-    for (var i=0; i<textoOriginal.length;i++)
+    for (var i=0; i<textoOriginal.length; i++)
     {
-      let x = textoOriginal.charCodeAt(i);
-      let newPosLetra = (x - 65 + clave) % 26 + 65;
+      let Posletra = textoOriginal.charCodeAt(i);
+      let newPosLetra = (Posletra - 65 + llave) % 26 + 65;
   		textoEncriptado += String.fromCharCode(newPosLetra);
     }
     return textoEncriptado;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-const botton = document.getElementById("encode").addEventListener("click", encriptacionCesar);
-function encriptacionCesar()
+document.getElementById("encode").addEventListener("click", codificacion);
+function codificacion()
 {
   var textoEncriptado = cipher.encode();
-  document.getElementById("textoEncriptado").value = textoEncriptado;
+  document.getElementById("solucion").value = textoEncriptado;
+}
+document.getElementById("decode").addEventListener("click", decodificacion);
+function decodificacion(){
+    var llave = parseInt(document.getElementById('candado').value);
+    var solucion = document.getElementById("solucion").value;
+    var desencriptado = "";
 
+    for (var i=0; i<solucion.length; i++)
+    {
+      let Posletra = solucion.charCodeAt(i);
+      let newPosLetra = (Posletra - 65 - llave) % 26 + 65;
+      desencriptado += String.fromCharCode(newPosLetra);
+    }
+    document.getElementById("textoDesencriptado").value = desencriptado;
 }
