0a66ade570fd5e2b6d1e13ae29bcaedda4256ead
Oshin Villegas <villegasoshin25@gmail.com>
2018-05-26 15:27:44 -0500

    esquemaok

--- src/cipher.js
+++ src/cipher.js
@@ -11,6 +11,34 @@ window.cipher = {
   		textoEncriptado += String.fromCharCode(newPosLetra);
     }
     return textoEncriptado;
+  },
+  decode:() => {
+    var llave = parseInt(document.getElementById('candado').value);
+    var solucion = document.getElementById("solucion").value ;
+    var desencriptado = "";
+
+    for (var i=0; i<solucion.length; i++)
+    {
+      let Posletra = solucion.charCodeAt(i);
+      let newPosLetra = (Posletra - 65 - llave) % 26 + 65;
+      desencriptado += String.fromCharCode(newPosLetra);
+    }
+    document.getElementById("textoDesencriptado").value;
+    return desencriptado;
+  },
+  createCipherWithOffset : () =>{
+    var llave = parseInt(document.getElementById('candado').value);
+    var solucion = document.getElementById("solucion").value ;
+    var desencriptado = "";
+
+    for (var i=0; i<solucion.length; i++)
+    {
+      let Posletra = solucion.charCodeAt(i);
+      let newPosLetra = (Posletra - 65 - llave) % 26 + 65;
+      desencriptado += String.fromCharCode(newPosLetra);
+    }
+    document.getElementById("textoDesencriptado").value;
+    return desencriptado;
   }
 
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 document.getElementById("encode").addEventListener("click", codificacion);
 function codificacion()
 {
+
   var textoEncriptado = cipher.encode();
   document.getElementById("solucion").value = textoEncriptado;
 }
 document.getElementById("decode").addEventListener("click", decodificacion);
-function decodificacion(){
-    var llave = parseInt(document.getElementById('candado').value);
-    var solucion = document.getElementById("solucion").value;
-    var desencriptado = "";
+function decodificacion()
+{
+  var desencriptado = cipher.decode();
+  document.getElementById("textoDesencriptado").value = desencriptado;
+}
 
-    for (var i=0; i<solucion.length; i++)
-    {
-      let Posletra = solucion.charCodeAt(i);
-      let newPosLetra = (Posletra - 65 - llave) % 26 + 65;
-      desencriptado += String.fromCharCode(newPosLetra);
-    }
-    document.getElementById("textoDesencriptado").value = desencriptado;
+document.getElementById("decode").addEventListener("click", decodificacion);
+function decodificacion()
+{
+  var desencriptado = cipher.createCipherWithOffset();
+  document.getElementById("textoDesencriptado").value = desencriptado;
 }
