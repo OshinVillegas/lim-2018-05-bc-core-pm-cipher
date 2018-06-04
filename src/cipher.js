3871e29e1690b9bf18710a1b740ea2c1effcb162
Oshin Villegas <villegasoshin25@gmail.com>
2018-06-04 14:39:35 -0500

    ProyectoCompletisimo

--- src/cipher.js
+++ src/cipher.js
@@ -26,24 +26,26 @@ window.cipher = {
   //Funcion decode que recibe offset Llave y string textoOriginal y retorna el texto desencriptado
   decode:(llave, textoOriginal) => {
     let desencriptado = "";
-
+  //Retrocede letra a letra el texto original 
     for (let i=0; i<textoOriginal.length; i++)
     {
+   //Obtener el codigo ASCII de la letra original
+
       let Posletra = textoOriginal.charCodeAt(i);
       let newPosLetra = Posletra;
-      if (Posletra >= 65 && Posletra <= 90)
+      if (Posletra >= 65 && Posletra <= 90) //Si es letra mayuscula
       {
         newPosLetra = (Posletra + 65 - llave) % 26 + 65;
       }
-      if (Posletra >= 97 && Posletra <= 122)
+      if (Posletra >= 97 && Posletra <= 122)//Si es letra minuscula
       
       {
         newPosLetra = (Posletra + 85 - llave) % 26 + 97;
       }
-      
+            //Agrega la letra desencriptada
       desencriptado += String.fromCharCode(newPosLetra);
     }
-
+      
     return desencriptado;
   },
   //Funcion createCipherWithOffset que recibe offset Llave y retorna un objeto con las funciones encode y decode
